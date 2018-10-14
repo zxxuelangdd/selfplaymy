d67df5f95c73a05978d27e932d34b45d91e13092
zxxuelangdd <18591965650@163.com>
2018-10-14 20:01:22 +0800

    test git

---
+++ src/main/java/gitTest/TestCommit.java
@@ -0,0 +1,13 @@
+package gitTest;
+
+/**
+ * @program: selfplay
+ * @description: 测试提交内容的修改
+ * @author: zx
+ * @create: 2018-10-14 19:56
+ **/
+public class TestCommit {
+    public static void main(String[] args) {
+        System.out.println("just  test git commit");
+    }
+}
