97f591886cb12886de5c2b505efa145102ea001e
zxxuelangdd <18591965650@163.com>
2018-10-14 22:47:09 +0800

    branch commit test2

--- src/main/java/gitTest/TestCommit.java
+++ src/main/java/gitTest/TestCommit.java
@@ -9,5 +9,6 @@ package gitTest;
 public class TestCommit {
     public static void main(String[] args) {
         System.out.println("just  test git commit");
+        System.out.println("branch add");
     }
 }
