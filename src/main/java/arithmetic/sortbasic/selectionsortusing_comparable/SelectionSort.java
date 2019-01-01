38eb42ba2ed00e52ab1355ef84cd4f186e1bb5d5
zxxuelangdd <18591965650@163.com>
2019-01-01 17:51:39 +0800

    格式化

--- src/main/java/arithmetic/sortbasic/selectionsortusing_comparable/SelectionSort.java
+++ src/main/java/arithmetic/sortbasic/selectionsortusing_comparable/SelectionSort.java
@@ -7,16 +7,16 @@ public class SelectionSort {
     }
 
     public static void sort(Comparable[] arr) {
-
         int n = arr.length;
         for (int i = 0; i < n; i++) {
             // 寻找[i, n)区间里的最小值的索引
             int minIndex = i;
+
             for (int j = i + 1; j < n; j++)
                 // 使用compareTo方法比较两个Comparable对象的大小
                 if (arr[j].compareTo(arr[minIndex]) < 0)
                     minIndex = j;
-
+            System.out.println("无用行");
             swap(arr, i, minIndex);
         }
     }
