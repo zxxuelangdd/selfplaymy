9cf934e7966f4c100e06a753d6a0c1ccecc2fdd1
zxxuelangdd <18591965650@163.com>
2019-05-04 15:24:54 +0800

    Merge branch 'test1'
    # Conflicts:
    #	.idea/workspace.xml

--- src/test/java/testOther/ListTest.java
+++ src/test/java/testOther/ListTest.java
@@ -1,3 +1,8 @@
+package testOther;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @program: selfplay
  * @description:
@@ -5,4 +10,13 @@
  * @create: 2019-05-04 15:15
  **/
 public class ListTest {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>(2);
+        strings.add("a");
+        strings.add("b");
+        strings.add("c");
+        strings.add("d");
+        strings.add("e");
+        System.out.println(strings);
+    }
 }
